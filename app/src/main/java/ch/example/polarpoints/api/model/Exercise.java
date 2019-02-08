3da1e5d3aed4d01663639830d55280ae9e558b3f
Karrie Moore <karrie.moore@ti8m.ch>
2019-02-08 16:45:20 +0100

    exercise model object

---
+++ app/src/main/java/ch/example/polarpoints/api/model/Exercise.java
@@ -0,0 +1,26 @@
+package ch.example.polarpoints.api.model;
+
+// json example:
+
+// { "upload-time": "2008-10-13T10:40:02.000Z",
+//  "id": 1937529874,
+//  "polar-user": "https://www.polaraccesslink/v3/users/1",
+//  "transaction-id": 179879,
+//  "device": "Polar M400",
+//  "start-time": "2008-10-13T10:40:02.000Z",
+//  "duration": "PT2H44M",
+//  "calories": 530,
+//  "distance": 1600,
+//  "heart-rate": {
+//    "average": 129,
+//    "maximum": 147
+//  },
+//  "training-load": 143.22,
+//  "sport": "OTHER",
+//  "has-route": true,
+//  "club-id": 999,
+//  "club-name": "Polar Club",
+//  "detailed-sport-info": "WATERSPORTS_WATERSKI" }
+
+public class Exercise {
+}
